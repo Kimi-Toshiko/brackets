2c107b4e2d0d0ac35cf10de1de6d03369d13d015
Kimi Toshiko <kimitoshiko@gmail.com>
2020-11-02 18:58:29 +0500

    feat: add solution without straight brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = new Array;
+
+  str.split('').map(el => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (el === bracketsConfig[i][0])
+        stack.push(el);
+      else {
+        if (stack[stack.length - 1] === bracketsConfig[i][0] && el === bracketsConfig[i][1])
+          stack.pop();
+      }
+    }
+  });
+
+  return stack.length === 0 ? true : false;
 }
